cb091bc11fecf78b0c19dea3a9637ac3621ff756
LuanaRTonelli <luana_tonelli@hotmail.com>
2025-11-21 14:07:06 -0500

    Implementation of Edit Camera (update)

---
+++ Security Devices Assets Mgmt/Views/CameraEditView.swift
@@ -0,0 +1,18 @@
+//
+//  CameraEditView.swift
+//  Security Devices Assets Mgmt
+//
+//  Created by user285344 on 11/20/25.
+//
+
+import SwiftUI
+
+struct CameraEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraEditView()
+}
